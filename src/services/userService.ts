1ed87d6063b5c80774165c4fe6812eec380ba0b3
Ashish19025 <Ashishkumar19025@gmail.com>
2026-06-13 23:23:39 +0530

    some more changes

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,22 +1,29 @@
-export const syncUser = async (
-  user: any
-) => {
+export const syncUser = async (user: {
+  clerkId: string;
+  firstName?: string;
+  lastName?: string;
+  email: string;
+}) => {
   try {
-    await fetch(
-      "http://192.168.1.5/api/users/sync",
+    const response = await fetch(
+      'http://192.168.1.105:5000/api/users/sync', // <-- Replace with your PC IP
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type":
-            "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(user),
       }
     );
+
+    const data = await response.json();
+
+    console.log('SYNC RESPONSE');
+    console.log(data);
+
+    return data;
   } catch (err) {
-    console.log(
-      "SYNC USER ERROR",
-      err
-    );
+    console.log('SYNC ERROR');
+    console.log(err);
   }
 };
